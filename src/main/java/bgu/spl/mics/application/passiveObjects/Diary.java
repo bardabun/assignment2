954e258696fd5acf488f64afe7ec94c223126c98
mamanyon <72416517+mamanyon@users.noreply.github.com>
2020-12-11 19:08:11 +0200

    Add files via upload

--- src/main/java/bgu/spl/mics/application/passiveObjects/Diary.java
+++ src/main/java/bgu/spl/mics/application/passiveObjects/Diary.java
@@ -9,4 +9,36 @@ package bgu.spl.mics.application.passiveObjects;
  */
 public class Diary {
 
+    private static class SingletonHolder {
+        private static Diary instance = new Diary();
+    }
+    public AtomicInteger totalAttacks = new AtomicInteger(0);
+    public long HanSoloFinish;
+    public long C3POFinish;
+    public long R2D2Deactivate;
+    public long LeiaTerminate;
+    public long HanSoloTerminate;
+    public long C3POTerminate;
+    public long R2D2Terminate;
+    public long LandoTerminate;
+
+
+    public static Diary getInstance(){
+        return SingletonHolder.instance;
+    }
+
+    private Diary(){
+    }
+
+
+    //debug
+    @Override
+    public String toString() {
+        String string1 = "totalAttacks:" + totalAttacks + "\n" + "HanSoloFinish:" + HanSoloFinish + "\n";
+        String string2 = "C3POFinish:" + C3POFinish + "\n" + "R2D2Deactivate:" + R2D2Deactivate + "\n";
+        String string3 = "LeiaTerminate:" + LeiaTerminate + "\n" + "HanSoloTerminate:" + HanSoloTerminate + "\n";
+        String string4 = "C3POTerminate:" + C3POTerminate + "\n" + "R2D2Terminate" + R2D2Terminate + "\n" + "LandoTerminate" + LandoTerminate + "\n";
+        return string1 + string2 + string3 + string4;
+    }
+
 }
